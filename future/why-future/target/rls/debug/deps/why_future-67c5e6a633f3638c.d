f2dcc981689857cf2cba6a28f1e344542b8023c4
hgfkeep <hgfkeep@gmail.com>
2020-05-12 10:22:19 +0800

    👌 Improve: clean middle files

--- future/why-future/target/rls/debug/deps/why_future-67c5e6a633f3638c.d
+++
@@ -1,5 +0,0 @@
-/Users/guangfuhe/Projects/rust/tutorial/future/why-future/target/rls/debug/deps/why_future-67c5e6a633f3638c.rmeta: src/main.rs
-
-/Users/guangfuhe/Projects/rust/tutorial/future/why-future/target/rls/debug/deps/why_future-67c5e6a633f3638c.d: src/main.rs
-
-src/main.rs:
--- future/why-future/target/rls/debug/deps/why_future-aa1903dc79521de9.d
+++
@@ -1,5 +0,0 @@
-/Users/guangfuhe/Projects/rust/tutorial/future/why-future/target/rls/debug/deps/why_future-aa1903dc79521de9.rmeta: src/main.rs
-
-/Users/guangfuhe/Projects/rust/tutorial/future/why-future/target/rls/debug/deps/why_future-aa1903dc79521de9.d: src/main.rs
-
-src/main.rs:
